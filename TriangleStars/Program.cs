594f825f90c7a92287c61d639e8ccaafafada744
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 15:25:09 +0300

    Initial commit

---
+++ TriangleStars/Program.cs
@@ -0,0 +1,17 @@
+﻿class TriangleStars
+{
+    public static void Main(string[] args)
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= size; i++)
+        {
+            for (int j = 1; j <=i; j++)
+                
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
